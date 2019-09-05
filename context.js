8ce37373593ac07956cc199143e51ade55494ecf
Daniel Marino <starzonmyarmz@gmail.com>
2019-09-05 09:31:15 -0400

    Some more robust form styles

--- context.js
+++ context.js
@@ -15,26 +15,32 @@ export default ({ setTempo, setTimeSignature, setVolume, tempo, volume }) => {
 
   return (
     <div className="context">
-      <div className="field">
-        <div className="label">
-          Slider Component
+      <div className="field-container">
+        <div className="field">
+          <div className="label">
+            Slider Component
+          </div>
+          <input type="range" min="0" max="1" step="0.05" value={volume} onChange={changeVolume} />
         </div>
-        <input type="range" min="0" max="1" step="0.05" value={volume} onChange={changeVolume} />
       </div>
-      <div className="field">
-        <div className="label">
-          Tempo
+
+      <div className="two-fields">
+        <div className="field">
+          <div className="label">
+            Tempo
+          </div>
+          <input type="number" className="input" min="40" max="200" step="1" value={tempo} onChange={changeTempo} />
         </div>
-        <input type="number" min="40" max="200" step="1" value={tempo} onChange={changeTempo} />
-      </div>
-      <div className="field">
-        <div className="label">
-          Time Signature
+
+        <div className="field">
+          <div className="label">
+            Time Signature
+          </div>
+          <select className="input select" onChange={changeTimeSignature}>
+            <option value="3/4">3/4</option>
+            <option value="4/4">4/4</option>
+          </select>
         </div>
-        <select onChange={changeTimeSignature}>
-          <option value="3/4">3/4</option>
-          <option value="4/4">4/4</option>
-        </select>
       </div>
     </div>
   )
