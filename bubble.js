b1c9d686b43c707729abc02cd3879646cad4bf37
Daniel Marino <starzonmyarmz@gmail.com>
2019-09-06 09:53:29 -0400

    Minor cleanup

--- bubble.js
+++ bubble.js
@@ -73,8 +73,8 @@ export default ({ onDelete, onSelect, selected, ac, bubble, tempo, timeSignature
 
     return (
       <button type="button" className="bubble-delete" onClick={onDelete}>
-        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24">
-          <path fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M16 8l-8 8m0-8l8 8"/>
+        <svg width="24" height="24" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2">
+          <path d="M16 8l-8 8m0-8l8 8"/>
         </svg>
       </button>
     )
